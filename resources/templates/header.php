25fd265c2b3011ecba19d1f2bda5c779a8ac05ef
hakasapl <hakansaplakog@gmail.com>
2020-01-24 03:58:23 +0000

    initial

---
+++ resources/templates/header.php
@@ -0,0 +1,16 @@
+<?php
+require_once "../resources/config.php";
+?>
+
+<html>
+
+<head>
+    <title>Unity Cluster Manager</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="css/light.css">
+    <link rel="stylesheet" type="text/css" href="css/page.css">
+    <link rel="stylesheet" type="text/css" href="css/context-menu.css">
+</head>
+
+<body>
---
+++ webroot/index.php
@@ -0,0 +1,96 @@
+<?php
+require_once "../resources/templates/header.php";
+?>
+
+<div class="mainWrapper">
+
+    <aside class="leftSide">
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+        </ul>
+    </aside>
+
+    <main class="rightSide">
+        <div class="pageHeader">
+            <span class="pageTitle center">Test Page</span>
+            <span class="pageDesc center">Test Description</span>
+        </div>
+
+        <div class="pageWrapper">
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras malesuada posuere lorem ut sollicitudin. Donec a sapien et urna posuere gravida. Pellentesque mollis lectus eu laoreet venenatis. Maecenas et orci orci. Duis lectus dolor, efficitur et ipsum quis, lacinia pharetra magna. In euismod sit amet diam nec dignissim. Sed mauris ligula, euismod vitae sagittis a, maximus rhoncus turpis. Etiam arcu ipsum, euismod maximus est in, congue luctus enim. Sed viverra purus quis mi malesuada, a tristique lorem rutrum. Sed ligula dui, placerat id velit ut, porttitor suscipit mi. Aenean sed turpis vitae tortor porta sodales. Ut eu elit odio. In eget turpis nec nulla fringilla interdum ac vitae risus. Mauris sed sapien eu mauris fringilla accumsan. Suspendisse volutpat leo dapibus lorem ullamcorper porta. In nisl lacus, lacinia at ante consequat, gravida viverra erat.<br><br><br><br><br><br><br>
+
+Donec vel magna sit amet risus convallis condimentum. Quisque augue magna, vestibulum in lectus ut, posuere accumsan odio. Cras finibus ut ex efficitur mattis. Nulla at malesuada eros, et rhoncus purus. Cras sit amet ipsum ex. Cras vitae eleifend dolor. Nulla accumsan felis nibh, ut faucibus eros fermentum at.
+
+Nunc ullamcorper suscipit sem at luctus. In a ultricies purus. In ornare diam maximus ipsum cursus, ac fermentum mi sollicitudin. Quisque pharetra tortor risus, quis pellentesque sapien mollis at. Sed tempus ut libero ac euismod. Nullam lobortis magna neque, consectetur laoreet tortor placerat vitae. Donec at sem ac purus elementum euismod sed vitae magna. Curabitur eleifend, turpis quis gravida tincidunt, orci sem porttitor sem, eu consequat metus arcu non augue. Praesent sit amet nulla aliquet, fringilla lorem id, consectetur augue.
+
+Cras quis molestie eros. Morbi non sem leo. Vivamus vitae mauris placerat, luctus leo vel, semper libero. Ut ac dui sollicitudin, convallis lorem laoreet, dictum enim. Donec eu felis neque. Fusce consequat sed libero nec porta. Aenean dignissim venenatis libero ac dictum. Curabitur pellentesque justo eget tortor tristique viverra. Ut quis tristique nibh, nec euismod libero. Praesent varius sapien tellus, eu ultricies nibh posuere non. Quisque a consectetur est, vitae aliquet purus. In hac habitasse platea dictumst. Suspendisse ut porta nibh. Donec sit amet leo eu dolor fermentum mattis. Fusce placerat ligula metus, et<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> consectetur ipsum vehicula in. Aenean pharetra purus eget dignissim feugiat.
+
+Vestibulum ornare posuere leo, ac semper est malesuada ut. Ut condimentum sapien mi, posuere aliquet tellus condimentum sed. Donec dapibus efficitur enim a fringilla. Aliquam tristique, nibh nec vehicula imperdiet, erat ante efficitur ex, sit amet pulvinar nisl tortor eget orci. Aliquam varius dui vitae tellus eleifend placerat sit amet at arcu. Suspendisse eu sapien sodales, pulvinar velit eget, ultrices libero. Suspendisse mauris metus, pharetra et purus facilisis, auctor laoreet nisl. Sed faucibus ante non dui aliquet, a scelerisque enim egestas.</span>
+        </div>
+    </main>
+
+</div>
+
+<nav id="foot_nav">
+    <a href="#">Menu 1</a>
+    <a href="#">Menu 2</a>
+    <a href="#">Menu 3</a>
+    <a class="rightBtn" href="#">Settings</a>
+</nav>
+
+<?php
+require_once "../resources/templates/footer.php";
+?>
